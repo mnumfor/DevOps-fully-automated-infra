859fade8b1732346e090e00e88a5b90263b60bc4
mercynumfor <numfor252@gmail.com>
2022-12-18 23:01:21 -0600

    code improvements

--- var.tf
+++ var.tf
@@ -1,7 +1,7 @@
 variable "key_pair_name" {
     type = string
     description = "keypair to utilize"
-    default = "Jenskins-pipeline"
+    default = "cici-pipeline-infra"
 }
 
 # variable "vpc_id" {
